4ff784e58e3c76441551c4f02912f847390238f8
ludovic <ludovic.racape@gmail.com>
2016-07-21 00:11:50 -0400

    Added World object and various KeyInputs

--- Is_It_A_RogueLike/src/screens/MapScreen.java
+++ Is_It_A_RogueLike/src/screens/MapScreen.java
@@ -3,16 +3,35 @@ package screens;
 import java.awt.event.KeyEvent;
 
 import asciiPanel.AsciiPanel;
+import graphics.World;
+import graphics.WorldBuilder;
 
 public class MapScreen implements Screen {
+	
+	private World world;
+    private int centerX;
+    private int centerY;
+    private int screenWidth;
+    private int screenHeight;
 
-	public MapScreen() {
-		// TODO Auto-generated constructor stub
+	public MapScreen(){
+        screenWidth = 80;
+        screenHeight = 21;
+        createWorld();
 	}
+	
+	private void createWorld(){
+        world = new WorldBuilder(90, 31)
+              .makeCaves()
+              .build();
+    }
 
 	@Override
 	public void displayOutput(AsciiPanel terminal) {
-        terminal.write("You are having fun.", 1, 1);
+		int left = getScrollX();
+        int top = getScrollY();   
+        displayTiles(terminal, left, top);
+        terminal.write('X', centerX - left, centerY - top);
         terminal.writeCenter("-- press [escape] to lose or [enter] to win --", 22);
 
 	}
@@ -21,10 +40,87 @@ public class MapScreen implements Screen {
 	public Screen respondToUserInput(KeyEvent key) {
         switch (key.getKeyCode()){
         case KeyEvent.VK_ESCAPE: return new GameOverScreen();
-        case KeyEvent.VK_ENTER: return new EndScreen();
+        case KeyEvent.VK_ENTER: return new EndScreen();       
+        case KeyEvent.VK_LEFT:
+        case KeyEvent.VK_H: scrollBy(-1, 0); break;
+        case KeyEvent.VK_RIGHT:
+        case KeyEvent.VK_L: scrollBy( 1, 0); break;
+        case KeyEvent.VK_UP:
+        case KeyEvent.VK_K: scrollBy( 0,-1); break;
+        case KeyEvent.VK_DOWN:
+        case KeyEvent.VK_J: scrollBy( 0, 1); break;
+        case KeyEvent.VK_Y: scrollBy(-1,-1); break;
+        case KeyEvent.VK_U: scrollBy( 1,-1); break;
+        case KeyEvent.VK_B: scrollBy(-1, 1); break;
+        case KeyEvent.VK_N: scrollBy( 1, 1); break;
+
         }
     
         return this;
+	}
+	
+	public int getScrollX() {
+	    return Math.max(0, Math.min(centerX - screenWidth / 2, world.getWidth() - screenWidth));
+	}
+	
+	public int getScrollY() {
+	    return Math.max(0, Math.min(centerY - screenHeight / 2, world.getHeight() - screenHeight));
+	}
+	
+	private void scrollBy(int mx, int my){
+        centerX = Math.max(0, Math.min(centerX + mx, world.getWidth() - 1));
+        centerY = Math.max(0, Math.min(centerY + my, world.getHeight() - 1));
+    }
+	
+	private void displayTiles(AsciiPanel terminal, int left, int top) {
+	    for (int x = 0; x < screenWidth; x++){
+	        for (int y = 0; y < screenHeight; y++){
+	            int wx = x + left;
+	            int wy = y + top;
+
+	            terminal.write(world.glyph(wx, wy), x, y, world.color(wx, wy));
+	        }
+	    }
+	}
+
+	public World getWorld() {
+		return world;
+	}
+
+	public void setWorld(World world) {
+		this.world = world;
+	}
+
+	public int getCenterX() {
+		return centerX;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+
+	public int getCenterY() {
+		return centerY;
+	}
+
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+
+	public int getScreenWidth() {
+		return screenWidth;
+	}
+
+	public void setScreenWidth(int screenWidth) {
+		this.screenWidth = screenWidth;
+	}
+
+	public int getScreenHeight() {
+		return screenHeight;
+	}
+
+	public void setScreenHeight(int screenHeight) {
+		this.screenHeight = screenHeight;
 	}
 
 }
