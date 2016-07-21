972dee2a27f7b90fc1b172b569031c5aa73f6b08
ludovic <ludovic.racape@gmail.com>
2016-07-21 14:05:01 -0400

    Addition an modifications
    Added creature and creatureFactory members
    Modified constructor to initialize creatureFactory and player
    Modified displayOutput method to display player character
    Modified respondToUserInput to handle player movements
    Modified getScrollX and getScrollY to center on the player

--- Is_It_A_RogueLike/src/screens/MapScreen.java
+++ Is_It_A_RogueLike/src/screens/MapScreen.java
@@ -3,14 +3,16 @@ package screens;
 import java.awt.event.KeyEvent;
 
 import asciiPanel.AsciiPanel;
+import bestiary.Creature;
+import bestiary.CreatureFactory;
 import graphics.World;
 import graphics.WorldBuilder;
 
 public class MapScreen implements Screen {
 	
 	private World world;
-    private int centerX;
-    private int centerY;
+	private CreatureFactory creatureFactory;
+	private Creature player;
     private int screenWidth;
     private int screenHeight;
 
@@ -18,8 +20,14 @@ public class MapScreen implements Screen {
         screenWidth = 80;
         screenHeight = 21;
         createWorld();
+        creatureFactory = new CreatureFactory(getWorld());
+        player = creatureFactory.newPlayer();
 	}
 	
+	/**
+	 * Initialize the world member of the MapScreen instance using the WorldBuilder methods
+	 * 
+	 */
 	private void createWorld(){
         world = new WorldBuilder(90, 31)
               .makeCaves()
@@ -31,7 +39,7 @@ public class MapScreen implements Screen {
 		int left = getScrollX();
         int top = getScrollY();   
         displayTiles(terminal, left, top);
-        terminal.write('X', centerX - left, centerY - top);
+        terminal.write(player.getGlyph(), player.getX() - left, player.getY() - top, player.getColor());
         terminal.writeCenter("-- press [escape] to lose or [enter] to win --", 22);
 
 	}
@@ -42,17 +50,17 @@ public class MapScreen implements Screen {
         case KeyEvent.VK_ESCAPE: return new GameOverScreen();
         case KeyEvent.VK_ENTER: return new EndScreen();       
         case KeyEvent.VK_LEFT:
-        case KeyEvent.VK_H: scrollBy(-1, 0); break;
+        case KeyEvent.VK_H: player.moveBy(-1, 0); break;
         case KeyEvent.VK_RIGHT:
-        case KeyEvent.VK_L: scrollBy( 1, 0); break;
+        case KeyEvent.VK_L: player.moveBy( 1, 0); break;
         case KeyEvent.VK_UP:
-        case KeyEvent.VK_K: scrollBy( 0,-1); break;
+        case KeyEvent.VK_K: player.moveBy( 0,-1); break;
         case KeyEvent.VK_DOWN:
-        case KeyEvent.VK_J: scrollBy( 0, 1); break;
-        case KeyEvent.VK_Y: scrollBy(-1,-1); break;
-        case KeyEvent.VK_U: scrollBy( 1,-1); break;
-        case KeyEvent.VK_B: scrollBy(-1, 1); break;
-        case KeyEvent.VK_N: scrollBy( 1, 1); break;
+        case KeyEvent.VK_J: player.moveBy( 0, 1); break;
+        case KeyEvent.VK_Y: player.moveBy(-1,-1); break;
+        case KeyEvent.VK_U: player.moveBy( 1,-1); break;
+        case KeyEvent.VK_B: player.moveBy(-1, 1); break;
+        case KeyEvent.VK_N: player.moveBy( 1, 1); break;
 
         }
     
@@ -60,17 +68,12 @@ public class MapScreen implements Screen {
 	}
 	
 	public int getScrollX() {
-	    return Math.max(0, Math.min(centerX - screenWidth / 2, world.getWidth() - screenWidth));
+	    return Math.max(0, Math.min(player.getX() - screenWidth / 2, world.getWidth() - screenWidth));
 	}
 	
 	public int getScrollY() {
-	    return Math.max(0, Math.min(centerY - screenHeight / 2, world.getHeight() - screenHeight));
+	    return Math.max(0, Math.min(player.getY() - screenHeight / 2, world.getHeight() - screenHeight));
 	}
-	
-	private void scrollBy(int mx, int my){
-        centerX = Math.max(0, Math.min(centerX + mx, world.getWidth() - 1));
-        centerY = Math.max(0, Math.min(centerY + my, world.getHeight() - 1));
-    }
 	
 	private void displayTiles(AsciiPanel terminal, int left, int top) {
 	    for (int x = 0; x < screenWidth; x++){
@@ -89,22 +92,22 @@ public class MapScreen implements Screen {
 
 	public void setWorld(World world) {
 		this.world = world;
+	}	
+
+	public CreatureFactory getCreatureFactory() {
+		return creatureFactory;
 	}
 
-	public int getCenterX() {
-		return centerX;
+	public void setCreatureFactory(CreatureFactory creatureFactory) {
+		this.creatureFactory = creatureFactory;
 	}
 
-	public void setCenterX(int centerX) {
-		this.centerX = centerX;
+	public Creature getPlayer() {
+		return player;
 	}
 
-	public int getCenterY() {
-		return centerY;
-	}
-
-	public void setCenterY(int centerY) {
-		this.centerY = centerY;
+	public void setPlayer(Creature player) {
+		this.player = player;
 	}
 
 	public int getScreenWidth() {
