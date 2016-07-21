201019a789a8edabd93666290a4b313f0fe1e21b
ludovic <ludovic.racape@gmail.com>
2016-07-21 00:13:57 -0400

    Added javadoc

--- Is_It_A_RogueLike/src/screens/Screen.java
+++ Is_It_A_RogueLike/src/screens/Screen.java
@@ -4,10 +4,21 @@ import java.awt.event.KeyEvent;
 
 import asciiPanel.AsciiPanel;
 
-public interface Screen {
+	public interface Screen {
 
-    public void displayOutput(AsciiPanel terminal);
+    /**
+     * method taking an AsciiPanel object as parameter and used to handle the different outputs on the screens
+     * 
+     * @param terminal
+     */
+    public abstract void displayOutput(AsciiPanel terminal);
 
-    public Screen respondToUserInput(KeyEvent key);
+    /**
+     * handles the different inputs of the user
+     * 
+     * @param key
+     * @return Screen object. Either a new type of screen or the same 
+     */
+    public abstract Screen respondToUserInput(KeyEvent key);
     
 }
