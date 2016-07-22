1f993e52c45a3e686c366ccefdca31fb7c8dce05
ludovic <ludovic.racape@gmail.com>
2016-07-22 17:38:43 -0400

    Modifed javadoc because eclipse generated an error

--- Is_It_A_RogueLike/src/screens/Screen.java
+++ Is_It_A_RogueLike/src/screens/Screen.java
@@ -9,14 +9,14 @@ import asciiPanel.AsciiPanel;
     /**
      * method taking an AsciiPanel object as parameter and used to handle the different outputs on the screens
      * 
-     * @param terminal
+     * @param terminal AsciiPanel object
      */
     public abstract void displayOutput(AsciiPanel terminal);
 
     /**
      * handles the different inputs of the user
      * 
-     * @param key
+     * @param key input
      * @return Screen object. Either a new type of screen or the same 
      */
     public abstract Screen respondToUserInput(KeyEvent key);
