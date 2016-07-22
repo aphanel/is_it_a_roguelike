9a82d2840f76206acac8b2fb0fd064c7f1a276f7
ludovic <ludovic.racape@gmail.com>
2016-07-22 17:39:26 -0400

    Created KoboldAi to handle kobold AI. Not doing much at the moment...

---
+++ Is_It_A_RogueLike/src/bestiary/KoboldAi.java
@@ -0,0 +1,18 @@
+package bestiary;
+
+import graphics.Tile;
+
+public class KoboldAi extends CreatureAi {
+
+	public KoboldAi(Creature creature) {
+		super(creature);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onEnter(int x, int y, Tile tile) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
